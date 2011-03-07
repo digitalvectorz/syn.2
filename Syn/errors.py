ee855aec719e79a3dbe7eb73043ea1d221e1ef93
Paul Tagliamonte <paultag@ubuntu.com>
2011-03-07 23:09:55 +0000

    frobernate

---
+++ Syn/errors.py
@@ -0,0 +1,34 @@
+#
+# Copyright (c) Paul Tagliamonte
+# GNU GPL-3+, 2011
+#
+
+class ArchiveNotFoundException(Exception):
+	def __init__(self, value):
+		self.value = value
+	def __str__(self):
+		return repr(self.value)
+
+class InvalidJSONException(Exception):
+	def __init__(self, value):
+		self.value = value
+	def __str__(self):
+		return repr(self.value)
+
+class InvalidArchiveException(Exception):
+	def __init__(self, value):
+		self.value = value
+	def __str__(self):
+		return repr(self.value)
+
+class NotAnArchiveException(Exception):
+	def __init__(self, value):
+		self.value = value
+	def __str__(self):
+		return repr(self.value)
+
+class FileNotPresentException(Exception):
+	def __init__(self, value):
+		self.value = value
+	def __str__(self):
+		return repr(self.value)
