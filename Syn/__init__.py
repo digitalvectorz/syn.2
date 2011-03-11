c54d0e2145a751429ea0a99c8b18e9dd11dd09d9
Paul Tagliamonte <paultag@ubuntu.com>
2011-03-11 20:21:45 +0000

    small improvements

---
+++ Syn/__init__.py
@@ -0,0 +1,12 @@
+#
+# Copyright (c) Paul Tagliamonte
+# GNU GPL-3+, 2011
+#
+
+appname     = "syn"
+description = "Syn Package Manager"
+version     = "0.1"
+
+def printVersion():
+	print "%s version %s" % ( appname, version )
+	print description
