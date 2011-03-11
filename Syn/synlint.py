bc44a5cae9ad5990cf3f57b75e6e30261309dfae
Paul Tagliamonte <paultag@ubuntu.com>
2011-03-11 01:43:21 +0000

    adding in lint checking. seems to work

--- Syn/synlint.py
+++ Syn/synlint.py
@@ -5,15 +5,32 @@
 
 import Syn.tarball as t
 import Syn.log     as l
+import Syn.Global  as g
+import Syn.policy
 
 
 def checkFields(attrs, meta):
-	pass
+	for a in attrs:
+		try:
+			good = meta[a]
+		except KeyError as e:
+			print "================================================================================"
+			print "          Missing Field: " + a
+			print "================================================================================"
+			print Syn.policy.DESCRS[a]
 
-def runCheck(tarball, verify = False ): # We're going to do a stricter check
+def runCheck(tarbal, verify = False ): # We're going to do a stricter check
 	try:
-		ar = t.archive(tarball)
-		metafile = ar.getConf(g.SYN_SRC_DIR + g.SYN_BUILDDIR_META])
-		
+		ar = t.archive(tarbal)
+		if ar.getClass() != t.BINARY:
+			l.l(l.CRITICAL,"Must be a binary package")
+			return -1
+
+		metafile = ar.getConf(g.SYN_BINARY_META)
+
+		checkFields(Syn.policy.META_REQUIRED,   metafile)
+		checkFields(Syn.policy.META_NEEDED,     metafile)
+		checkFields(Syn.policy.META_GOODTOHAVE, metafile)
+
 	except Syn.errors.SynException as e:
 		l.l(l.CRITICAL, str(e))
