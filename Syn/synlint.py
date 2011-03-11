7055a601dc43c159011c5eaa2b87fc690732c394
Paul Tagliamonte <paultag@ubuntu.com>
2011-03-11 01:52:01 +0000

    a little whining never hurt anyone

--- Syn/synlint.py
+++ Syn/synlint.py
@@ -10,6 +10,7 @@ import Syn.policy
 
 
 def checkFields(attrs, meta):
+	errs = 0
 	for a in attrs:
 		try:
 			good = meta[a]
@@ -18,6 +19,8 @@ def checkFields(attrs, meta):
 			print "          Missing Field: " + a
 			print "================================================================================"
 			print Syn.policy.DESCRS[a]
+			errs += 1
+	return errs
 
 def runCheck(tarbal, verify = False ): # We're going to do a stricter check
 	try:
@@ -28,9 +31,32 @@ def runCheck(tarbal, verify = False ): # We're going to do a stricter check
 
 		metafile = ar.getConf(g.SYN_BINARY_META)
 
-		checkFields(Syn.policy.META_REQUIRED,   metafile)
-		checkFields(Syn.policy.META_NEEDED,     metafile)
-		checkFields(Syn.policy.META_GOODTOHAVE, metafile)
+		r_errs = checkFields(Syn.policy.META_REQUIRED,   metafile)
+		n_errs = checkFields(Syn.policy.META_NEEDED,     metafile)
+		g_errs = checkFields(Syn.policy.META_GOODTOHAVE, metafile)
+
+		sane = False
+
+		if r_errs + n_errs == 0:
+			sane = True
+
+		l.l(l.MESSAGE,"Errors:")
+		l.l(l.MESSAGE,"")
+		l.l(l.MESSAGE,"  Serious:  " + str(r_errs))
+		l.l(l.MESSAGE,"Important:  " + str(n_errs))
+		l.l(l.MESSAGE," Pedantic:  " + str(g_errs))
+		l.l(l.MESSAGE,"")
+		if sane:
+			l.l(l.MESSAGE,"This package is acceptable to build. Check with")
+			l.l(l.MESSAGE," your friendly project developer about it's archive")
+			l.l(l.MESSAGE," status.")
+		else:
+			l.l(l.MESSAGE,"This package is *NOT* acceptable for something as")
+			l.l(l.MESSAGE," simple as a safe build and install. Please fix the")
+			l.l(l.MESSAGE," issues above.")
+		l.l(l.MESSAGE,"")
+
+		return ( sane, r_errs, n_errs, g_errs )
 
 	except Syn.errors.SynException as e:
 		l.l(l.CRITICAL, str(e))
